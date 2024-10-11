ee225289ba7b74f885582159ab3b57185e2845c3
UdayKiran <uday@hexacluster.ai>
2024-10-11 17:14:13 +0530

    added express rate limit in main service

--- main-service/src/app.ts
+++ main-service/src/app.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 import apiRoutes from "./routes";
 import errorHandler from "./utils/error.handler";
 import requestLogger from "./utils/request-logger";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(requestLogger);
+app.use(limiter);
 
 // routes
 app.use("/api", apiRoutes);
